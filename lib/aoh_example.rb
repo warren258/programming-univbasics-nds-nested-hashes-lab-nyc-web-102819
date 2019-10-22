801c4ab2a2bc8d4b5d78950ad13d76abfa904bda
Warren McDonald <warrenjmac@mail.com>
2019-10-22 12:12:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [DON_G =>]
+  [DON_G = ]
 end
 
 def aoh_lookup(aoh, row, key)
