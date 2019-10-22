c03e77e718475de37873d8e5e0478332c232a639
Warren McDonald <warrenjmac@mail.com>
2019-10-22 12:30:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,8 @@ def literal_aoh
   # held in the constants
   [{name:  "Don Gately", occupation: "Live-in Staff"}, 
   {name:  "Joelle van Dyne", occupation: "Radio Personality"}, 
-  {}, {}, {}]
+  {name:  "Pat Monteseian", occupation: "Staff"}, 
+  {}, {}]
 end
 
 def aoh_lookup(aoh, row, key)
